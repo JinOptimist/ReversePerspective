4149186a4928fbef2eb3bab0001e03091edc2f6b
Pavel Lvou <Pavel_Lvou@epam.com>
2015-12-10 15:42:08 +0300

    'init'

---
+++ ReversePerspective/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Optimization;
+
+namespace ReversePerspective
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                      "~/Scripts/angular.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").IncludeDirectory(
+                      "~/Scripts/app", "*.js", true));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ ReversePerspective/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReversePerspective.MvcApplication" Language="C#" %>
